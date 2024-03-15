7fcc9329a1561ddfde737b23b7c693e5f3737195
Aniket Kumar Roy <aniketroy769@gmail.com>
2024-03-15 17:35:15 +0530

    added queue data structure for storing instruction bytes for later formating

--- risc-v-asm/include/utilities.h
+++ risc-v-asm/include/utilities.h
@@ -5,4 +5,17 @@ void RemoveSpaces(char **line);
 void RemoveComments(char *line, char *comment);
 void Preprocess(char **line, char *comment);
 void RegisterCode(char* r, char *binary, void (*error)(char*, int));
+struct Queue{
+    char** arr;
+    int str_size;
+    int size;
+    int r;
+    int f;
+};
+int isFull(struct Queue* q);
+int isEmpty(struct Queue* q);
+void enqueue(struct Queue* q, char* val);
+char* dequeue(struct Queue* q);
+void InitializeQueue(struct Queue *q, int size, int str_size);
+void DeleteQueue(struct Queue *q);
 #endif
--- risc-v-asm/utilities.c
+++ risc-v-asm/utilities.c
@@ -48,3 +48,45 @@ void RegisterCode(char* r, char *binary, void (*error)(char*, int)){
     }
     ValueInterpreter(r+1, binary, 5, error);
 }
+int isFull(struct Queue* q){
+    if ((q->r+1)%q->size == q->f)
+    return 1;
+    return 0;
+}
+int isEmpty(struct Queue* q){
+    if ((q->f)%q->size == q->r)
+    return 1;
+    return 0;
+}
+
+void enqueue(struct Queue* q, char* val){
+    if (!isFull(q)){
+        q->r = (q->r + 1)%q->size;
+        strcpy(q->arr[q->r], val);
+    }
+}
+char* dequeue(struct Queue* q){
+    // don't worry if we are returning a pointer, this pointer is referring to a 
+    // initialized location in heap, so it is not a dangling pointer
+    if (!isEmpty(q)){
+        q->f = (q->f + 1)%q->size;
+        char* val = q->arr[q->f];
+        return val;
+    }
+    return NULL;
+}
+void InitializeQueue(struct Queue *q, int size, int str_size){
+    q->size = size;
+    q->r = size - 1;
+    q->f = size - 1;
+    q->arr = (char**)malloc(q->size*sizeof(char**));
+    q->str_size = str_size; // 8(1 for each 8 binary bits) + 1(null character)
+    for(int i = 0; i< q->size; i++)
+    q->arr[i] = (char*)malloc(q->str_size*sizeof(char*));
+}
+void DeleteQueue(struct Queue *q){
+    for(int i = 0; i< q->size; i++)
+    free(q->arr[i]);
+
+    free(q->arr);
+}
